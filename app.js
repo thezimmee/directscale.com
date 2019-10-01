ff9846995b6f69860a88c0c065929426fc5bf6d3
Zimmee <zimmee@ross.localdomain>
2019-09-30 22:20:33 -0600

    Updates

--- app.js
+++ app.js
@@ -1,1 +1,45 @@
-!function(){"use strict";new class{constructor(t){this.options=Object.assign({toggleSelector:".hamburger",pageSelector:".page",activeClass:"off-canvas--is-open"},t||{}),this.dom={toggle:document.querySelector(this.options.toggleSelector),page:document.querySelector(this.options.pageSelector)},this.dom.toggle.addEventListener("click",this.toggle.bind(this))}toggle(){this.dom.page.classList.toggle(this.options.activeClass)}open(){this.dom.page.classList.add(this.options.activeClass)}close(){this.dom.page.classList.remove(this.options.activeClass)}destroy(){this.toggle.removeEventListener("click",this.toggle)}}}();
+/*! DirectScale.com.js | @author DirectScale | @reference https://directscale.com */
+
+(function () {
+  'use strict';
+
+  class OffCanvasNav {
+    constructor(options) {
+      this.options = Object.assign({
+        toggleSelector: '.hamburger',
+        pageSelector: '.page',
+        activeClass: 'off-canvas--is-open'
+      }, options || {});
+      this.dom = {
+        toggle: document.querySelector(this.options.toggleSelector),
+        page: document.querySelector(this.options.pageSelector)
+      };
+      this.dom.toggle.addEventListener('click', this.toggle.bind(this));
+    }
+
+    toggle() {
+      this.dom.page.classList.toggle(this.options.activeClass);
+    }
+
+    open() {
+      this.dom.page.classList.add(this.options.activeClass);
+    }
+
+    close() {
+      this.dom.page.classList.remove(this.options.activeClass);
+    }
+
+    destroy() {
+      this.toggle.removeEventListener('click', this.toggle);
+    }
+
+  }
+
+  /* eslint-disable-next-line */
+
+  var app = {
+    nav: new OffCanvasNav()
+  };
+
+}());
+//# sourceMappingURL=app.js.map
--- index.legacy.js
+++ index.legacy.js
@@ -1,1 +1,338 @@
-!function(){"use strict";function s(s,e){this.options=Object.assign({},this.defaults,e||{}),"number"!=typeof this.options.autoplay&&(this.options.autoplay=!1),this.index=0,this.offset=0,this.timer=null,this.offsetProp="offsetLeft",this.sizeProp="offsetWidth",this.options.vertical&&(this.offsetProp="offsetTop",this.sizeProp="offsetHeight"),this.max={index:0,slide:null,offset:0,maxed:!1},this._events={},this.init(s)}s.prototype={defaults:{autoplay:6e3,start:0,infinite:!0,vertical:!1,dots:!0,nextHtml:'<svg viewBox="0 0 24 24"><path d="M7.41 8.58L12 13.17l4.59-4.59L18 10l-6 6-6-6 1.41-1.42z"/></svg>',previousHtml:'<svg viewBox="0 0 24 24"><path d="M7.41 8.58L12 13.17l4.59-4.59L18 10l-6 6-6-6 1.41-1.42z"/></svg>',rootClass:"slider",verticalClass:"slider--vertical",stageClass:"slider__stage",sliderClass:"slider__slides",slideClass:"slider__slide",previousClass:"slider__previous",nextClass:"slider__next",dotsClass:"slider__dots",dotClass:"slider__dot",playToggleClass:"play-toggle",playingClass:"play-toggle--playing",activeSlideClass:"slider__slide--is-active",activeDotClass:"slider__dot--is-active"},buildDom:function(s){var e=this;e.dom={},e.dom.slider=s instanceof HTMLElement?s:document.querySelector(s),s=e.dom.slider,e.options=Object.assign(e.options,function(){var s=arguments.length>0&&void 0!==arguments[0]?arguments[0]:{};return Object.keys(s).forEach((function(e){"true"===s[e]||""===s[e]?s[e]=!0:"false"===s[e]?s[e]=!1:"["===s[e][0]&&"]"===s[e][s[e].length-1]||"{"===s[e][0]&&"}"===s[e][s[e].length-1]?s[e]=JSON.parse(s[e]):isNaN(+s[e])||(s[e]=+s[e]),e.indexOf("-")>-1&&(s[e.replace(/-([a-z])/g,(function(s,e){return e.toUpperCase()}))]=s[e],delete s[e])})),s}(Object.assign({},s.dataset))),s.classList.add(e.options.rootClass),e.dom.slider=s.querySelector("."+e.options.sliderClass),e.dom.slides=(e.dom.slider||s).children,e.dom.stage=s.querySelector("."+e.options.stageClass),e.dom.next=s.querySelector("."+e.options.nextClass),e.dom.previous=s.querySelector("."+e.options.previousClass),e.dom.dots=s.querySelector("."+e.options.dotsClass),e.dom.slider||(e.dom.slider=document.createElement("div"),e.dom.slider.classList.add(e.options.sliderClass));var t=e.dom.slider.children.length;Array.from(e.dom.slides).forEach((function(s){s.classList.add(e.options.slideClass),t&&e.dom.slider.appendChild(s)})),e.dom.stage||(e.dom.stage=document.createElement("div"),e.dom.stage.classList.add(e.options.stageClass),s.appendChild(e.dom.stage)),e.dom.stage.querySelector("."+e.options.sliderClass)||e.dom.stage.appendChild(e.dom.slider),e.options.vertical&&s.classList.add(e.options.verticalClass),e.dom.previous||(e.dom.previous=document.createElement("a"),e.dom.previous.classList.add(e.options.previousClass),e.dom.previous.innerHTML=e.options.previousHtml,s.appendChild(e.dom.previous)),e.dom.next||(e.dom.next=document.createElement("a"),e.dom.next.classList.add(e.options.nextClass),e.dom.next.innerHTML=e.options.nextHtml,s.appendChild(e.dom.next)),e.options.dots&&!e.options.vertical&&(e.dom.dots||(e.dom.dots=document.createElement("div"),e.dom.dots.classList.add(e.options.dotsClass),s.appendChild(e.dom.dots)),e.dom.dots.children.length||Array.from(e.dom.slides).forEach((function(s,t){var i=document.createElement("a");i.classList.add(e.options.dotClass),i.dataset.sliderSlide=t,i.addEventListener("click",(function(s){return e.goTo(t)})),e.dom.dots.appendChild(i)})))},refresh:function(){var s,e=this;e.max.index=e.dom.slides.length-1,s=e.dom.slides[e.max.index],e.max.offset=s[e.offsetProp]+s[e.sizeProp]-e.dom.stage[e.sizeProp],Array.from(e.dom.slides).forEach((function(s){s.dataset.sliderOffset=s[e.offsetProp]+e.offset})),e.goTo(e.index)},goTo:function(s){void 0===s||"next"===s?s=this._nextSlide():"previous"!==s&&"prev"!==s||(s=this._previousSlide()),this.dom.slides[this.index].classList.remove(this.options.activeSlideClass),this.options.dots&&this.dom.dots&&this.dom.dots.children[this.index].classList.remove(this.options.activeDotClass),this.dom.slides[s].classList.add(this.options.activeSlideClass),this.options.dots&&this.dom.dots&&this.dom.dots.children[s].classList.add(this.options.activeDotClass),this.offset=parseInt(this.dom.slides[s].dataset.sliderOffset,10),!this.options.infinite&&this.offset>=this.max.offset?(this.max.maxed||(this.index=s),this.max.maxed=!0,this.offset=this.max.offset):(this.max.maxed=!1,this.index=s),this.options.infinite||(this.index===this.dom.slides.length-1||this.max.maxed?this.dom.next.setAttribute("disabled",!0):this.dom.next.removeAttribute("disabled"),0===this.index?this.dom.previous.setAttribute("disabled",!0):this.dom.previous.removeAttribute("disabled")),this.dom.slider.style[this.options.vertical?"marginTop":"marginLeft"]="-"+this.offset+"px",this.timer&&(this.pause(),this.play())},play:function(){var s=this;s.timer||"number"!=typeof s.options.autoplay||(s.timer=setInterval((function(){s.goTo()}),s.options.autoplay))},pause:function(){clearInterval(this.timer),this.timer=null},init:function(s){this.buildDom(s),this._addListeners(),this.refresh(),this.goTo(this.options.start),this.options.autoplay&&this.play()},destroy:function(){this._removeListeners()},_previousSlide:function(){return this.index<=0?this.options.infinite?this.dom.slides.length-1:0:this.index-1},_nextSlide:function(){return this.index>=this.max.index?this.options.infinite?0:this.max.index:this.index+1},_addListeners:function(){var s,e,t,i,o=this;o._addEvent("previous","click",(function(s){if(!o.dom.previous.hasAttribute("disabled"))return o.goTo("previous")})),o._addEvent("next","click",(function(s){if(!o.dom.next.hasAttribute("disabled"))return o.goTo("next")})),o._addEvent("slider","mouseenter",(function(){o.pause()})),o._addEvent("slider","mouseleave",(function(){o.play()})),o._events["window.resize"]=(s=function(){o.refresh()},void 0===(e=100)&&(e=100),function(){var o=this,d=arguments,n=function(){i=null,t||s.apply(o,d)},a=t&&!i;clearTimeout(i),i=setTimeout(n,e),a&&s.apply(o,d)}),window.addEventListener("resize",o._events["window.resize"])},_removeListeners:function(){var s=this;window.removeEventListener("resize",s._events["window.resize"]),delete s._events["window.resize"],Object.keys(s._events).forEach((function(e){var t=e.split(".");s.dom[t[0]].removeEventListener(t[1],s._events[e]),delete s._events[e]}))},_addEvent:function(s,e,t){var i=[s,e].join(".");this._events[i]||(this._events[i]=t),this.dom[s].addEventListener(e,t)},_removeEvent:function(s,e){var t=[s,e].join(".");this._events[t]&&(this.dom[s].removeEventListener(e,this._events[t]),delete this._events[t])}};new s(".testimonials")}();
+/*! DirectScale.com.js | @author DirectScale | @reference https://directscale.com */
+
+(function () {
+  'use strict';
+
+  function debounce(callback, wait, immediate) {
+    if (wait === undefined) wait = 100;
+    var timeout;
+    return function () {
+      var context = this;
+      var args = arguments;
+
+      var later = function later() {
+        timeout = null;
+        if (!immediate) callback.apply(context, args);
+      };
+
+      var callNow = immediate && !timeout;
+      clearTimeout(timeout);
+      timeout = setTimeout(later, wait);
+      if (callNow) callback.apply(context, args);
+    };
+  }
+
+  function deserialize() {
+    var data = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : {};
+    Object.keys(data).forEach(function (key) {
+      // Boolean types.
+      if (data[key] === 'true' || data[key] === '') data[key] = true;else if (data[key] === 'false') data[key] = false; // Object/Array types.
+      else if (data[key][0] === '[' && data[key][data[key].length - 1] === ']' || data[key][0] === '{' && data[key][data[key].length - 1] === '}') data[key] = JSON.parse(data[key]); // Number types.
+        else if (!isNaN(+data[key])) data[key] = +data[key]; // Convert dash-case to camelCase.
+
+      if (key.indexOf('-') > -1) {
+        data[key.replace(/-([a-z])/g, function (m, w) {
+          return w.toUpperCase();
+        })] = data[key];
+        delete data[key];
+      }
+    });
+    return data;
+  }
+
+  /* globals HTMLElement */
+  Slider.prototype = {
+    defaults: {
+      // Options:
+      autoplay: 6000,
+      start: 0,
+      infinite: true,
+      vertical: false,
+      dots: true,
+      nextHtml: '<svg viewBox="0 0 24 24"><path d="M7.41 8.58L12 13.17l4.59-4.59L18 10l-6 6-6-6 1.41-1.42z"/></svg>',
+      previousHtml: '<svg viewBox="0 0 24 24"><path d="M7.41 8.58L12 13.17l4.59-4.59L18 10l-6 6-6-6 1.41-1.42z"/></svg>',
+      // Classes:
+      rootClass: 'slider',
+      verticalClass: 'slider--vertical',
+      stageClass: 'slider__stage',
+      sliderClass: 'slider__slides',
+      slideClass: 'slider__slide',
+      previousClass: 'slider__previous',
+      nextClass: 'slider__next',
+      dotsClass: 'slider__dots',
+      dotClass: 'slider__dot',
+      playToggleClass: 'play-toggle',
+      playingClass: 'play-toggle--playing',
+      activeSlideClass: 'slider__slide--is-active',
+      activeDotClass: 'slider__dot--is-active'
+    },
+    buildDom: function buildDom(element) {
+      var slider = this;
+      slider.dom = {}; // Assign root element.
+
+      slider.dom.slider = element instanceof HTMLElement ? element : document.querySelector(element);
+      element = slider.dom.slider;
+      slider.options = Object.assign(slider.options, deserialize(Object.assign({}, element.dataset)));
+      element.classList.add(slider.options.rootClass); // Check for existing dom elements (if not found, they will be created later).
+
+      slider.dom.slider = element.querySelector('.' + slider.options.sliderClass);
+      slider.dom.slides = (slider.dom.slider || element).children;
+      slider.dom.stage = element.querySelector('.' + slider.options.stageClass);
+      slider.dom.next = element.querySelector('.' + slider.options.nextClass);
+      slider.dom.previous = element.querySelector('.' + slider.options.previousClass);
+      slider.dom.dots = element.querySelector('.' + slider.options.dotsClass); // Create dom.slider element.
+
+      if (!slider.dom.slider) {
+        slider.dom.slider = document.createElement('div');
+        slider.dom.slider.classList.add(slider.options.sliderClass);
+      } // Ensure slides are inside of dom.slider.
+
+
+      var appendToSlider = slider.dom.slider.children.length;
+      Array.from(slider.dom.slides).forEach(function (slide) {
+        slide.classList.add(slider.options.slideClass);
+        if (appendToSlider) slider.dom.slider.appendChild(slide);
+      }); // Create stage, which is useful for styling.
+
+      if (!slider.dom.stage) {
+        slider.dom.stage = document.createElement('div');
+        slider.dom.stage.classList.add(slider.options.stageClass);
+        element.appendChild(slider.dom.stage);
+      } // Append slides inside of the stage.
+
+
+      if (!slider.dom.stage.querySelector('.' + slider.options.sliderClass)) {
+        slider.dom.stage.appendChild(slider.dom.slider);
+      } // Check if it's vertical.
+
+
+      if (slider.options.vertical) {
+        element.classList.add(slider.options.verticalClass);
+      } // Add previous control.
+
+
+      if (!slider.dom.previous) {
+        slider.dom.previous = document.createElement('a');
+        slider.dom.previous.classList.add(slider.options.previousClass);
+        slider.dom.previous.innerHTML = slider.options.previousHtml;
+        element.appendChild(slider.dom.previous);
+      } // Add next control.
+
+
+      if (!slider.dom.next) {
+        slider.dom.next = document.createElement('a');
+        slider.dom.next.classList.add(slider.options.nextClass);
+        slider.dom.next.innerHTML = slider.options.nextHtml;
+        element.appendChild(slider.dom.next);
+      } // Add dot controls.
+
+
+      if (slider.options.dots && !slider.options.vertical) {
+        if (!slider.dom.dots) {
+          slider.dom.dots = document.createElement('div');
+          slider.dom.dots.classList.add(slider.options.dotsClass);
+          element.appendChild(slider.dom.dots);
+        }
+
+        if (!slider.dom.dots.children.length) {
+          Array.from(slider.dom.slides).forEach(function (slide, i) {
+            var dot = document.createElement('a');
+            dot.classList.add(slider.options.dotClass);
+            dot.dataset.sliderSlide = i;
+            dot.addEventListener('click', function (event) {
+              return slider.goTo(i);
+            });
+            slider.dom.dots.appendChild(dot);
+          });
+        }
+      }
+    },
+    refresh: function refresh() {
+      var slider = this;
+      var maxSlide;
+      slider.max.index = slider.dom.slides.length - 1;
+      maxSlide = slider.dom.slides[slider.max.index];
+      slider.max.offset = maxSlide[slider.offsetProp] + maxSlide[slider.sizeProp] - slider.dom.stage[slider.sizeProp];
+      Array.from(slider.dom.slides).forEach(function (slide) {
+        slide.dataset.sliderOffset = slide[slider.offsetProp] + slider.offset;
+      });
+      slider.goTo(slider.index);
+    },
+    goTo: function goTo(slide) {
+      var slider = this; // Convert slide to the slide's index number.
+
+      if (slide === undefined || slide === 'next') {
+        slide = slider._nextSlide();
+      } else if (slide === 'previous' || slide === 'prev') {
+        slide = slider._previousSlide();
+      } // Deactivate current slide.
+
+
+      slider.dom.slides[slider.index].classList.remove(slider.options.activeSlideClass);
+      if (slider.options.dots && slider.dom.dots) slider.dom.dots.children[slider.index].classList.remove(slider.options.activeDotClass); // Activate new slide.
+
+      slider.dom.slides[slide].classList.add(slider.options.activeSlideClass);
+      if (slider.options.dots && slider.dom.dots) slider.dom.dots.children[slide].classList.add(slider.options.activeDotClass); // Set offset. Cap it to the size of all slides minus the size of the stage.
+
+      slider.offset = parseInt(slider.dom.slides[slide].dataset.sliderOffset, 10);
+
+      if (!slider.options.infinite && slider.offset >= slider.max.offset) {
+        if (!slider.max.maxed) slider.index = slide;
+        slider.max.maxed = true;
+        slider.offset = slider.max.offset;
+      } else {
+        slider.max.maxed = false;
+        slider.index = slide;
+      } // Disable next/previous as needed.
+
+
+      if (!slider.options.infinite) {
+        if (slider.index === slider.dom.slides.length - 1 || slider.max.maxed) {
+          slider.dom.next.setAttribute('disabled', true);
+        } else {
+          slider.dom.next.removeAttribute('disabled');
+        }
+
+        if (slider.index === 0) {
+          slider.dom.previous.setAttribute('disabled', true);
+        } else {
+          slider.dom.previous.removeAttribute('disabled');
+        }
+      } // Update the stage's margin.
+
+
+      slider.dom.slider.style[slider.options.vertical ? 'marginTop' : 'marginLeft'] = '-' + slider.offset + 'px'; // Reset timer.
+
+      if (slider.timer) {
+        slider.pause();
+        slider.play();
+      }
+    },
+    play: function play() {
+      var slider = this;
+      if (slider.timer || typeof slider.options.autoplay !== 'number') return;
+      slider.timer = setInterval(function () {
+        slider.goTo();
+      }, slider.options.autoplay);
+    },
+    pause: function pause() {
+      var slider = this;
+      clearInterval(slider.timer);
+      slider.timer = null;
+    },
+    init: function init(element) {
+      var slider = this; // Build the dom.
+
+      slider.buildDom(element); // Add events.
+
+      slider._addListeners(); // Refresh the offsets.
+
+
+      slider.refresh(); // Kick it off.
+
+      slider.goTo(slider.options.start);
+      if (slider.options.autoplay) slider.play();
+    },
+    destroy: function destroy() {
+      this._removeListeners();
+    },
+    _previousSlide: function _previousSlide() {
+      return this.index <= 0 ? this.options.infinite ? this.dom.slides.length - 1 : 0 : this.index - 1;
+    },
+    _nextSlide: function _nextSlide() {
+      return this.index >= this.max.index ? this.options.infinite ? 0 : this.max.index : this.index + 1;
+    },
+    _addListeners: function _addListeners() {
+      var slider = this; // Previous control click.
+
+      slider._addEvent('previous', 'click', function (event) {
+        if (slider.dom.previous.hasAttribute('disabled')) return;
+        return slider.goTo('previous');
+      }); // Next control click.
+
+
+      slider._addEvent('next', 'click', function (event) {
+        if (slider.dom.next.hasAttribute('disabled')) return;
+        return slider.goTo('next');
+      }); // Add autoplay events.
+
+
+      slider._addEvent('slider', 'mouseenter', function () {
+        slider.pause();
+      });
+
+      slider._addEvent('slider', 'mouseleave', function () {
+        slider.play();
+      }); // Add window resize.
+
+
+      slider._events['window.resize'] = debounce(function () {
+        slider.refresh();
+      }, 100);
+      window.addEventListener('resize', slider._events['window.resize']);
+    },
+    _removeListeners: function _removeListeners() {
+      var slider = this; // Remove window resize.
+
+      window.removeEventListener('resize', slider._events['window.resize']);
+      delete slider._events['window.resize']; // Remove other cached events.
+
+      Object.keys(slider._events).forEach(function (id) {
+        var key = id.split('.');
+        slider.dom[key[0]].removeEventListener(key[1], slider._events[id]);
+        delete slider._events[id];
+      });
+    },
+    _addEvent: function _addEvent(name, event, fn) {
+      var slider = this;
+      var id = [name, event].join('.');
+      if (!slider._events[id]) slider._events[id] = fn;
+      slider.dom[name].addEventListener(event, fn);
+    },
+    _removeEvent: function _removeEvent(name, event) {
+      var slider = this;
+      var id = [name, event].join('.');
+
+      if (slider._events[id]) {
+        slider.dom[name].removeEventListener(event, slider._events[id]);
+        delete slider._events[id];
+      }
+    }
+  };
+  /**
+   * Slider constructor.
+   */
+
+  function Slider(element, options) {
+    // Set options.
+    this.options = Object.assign({}, this.defaults, options || {});
+    if (typeof this.options.autoplay !== 'number') this.options.autoplay = false; // Set up initial state.
+
+    this.index = 0;
+    this.offset = 0;
+    this.timer = null;
+    this.offsetProp = 'offsetLeft';
+    this.sizeProp = 'offsetWidth';
+
+    if (this.options.vertical) {
+      this.offsetProp = 'offsetTop';
+      this.sizeProp = 'offsetHeight';
+    }
+
+    this.max = {
+      index: 0,
+      slide: null,
+      offset: 0,
+      maxed: false
+    };
+    this._events = {}; // Initialize this slider.
+
+    this.init(element);
+  }
+
+  var home = {
+    slider: new Slider('.testimonials')
+  };
+
+}());
+//# sourceMappingURL=index.legacy.js.map
